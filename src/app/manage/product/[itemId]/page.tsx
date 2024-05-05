53053751a5277bcf01573a8dce8b1ea309529aaf
user <>
2024-05-05 17:59:34 +0900

    Fix: 빌드오류 수정
    page.tsx 코드 save 적용

---
+++ src/app/manage/product/[itemId]/page.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function page() {
+  return <div>등록 상품 상세 조회/수정 페이지</div>;
+}
---
+++ src/app/manage/product/upload/page.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function page() {
+  return <div>상품 업로드 페이지</div>;
+}
