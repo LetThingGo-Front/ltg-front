c684642f2a28a779445f7ea85cac03c834a78bef
user <>
2024-06-01 21:45:23 +0900

    Feat: 로그인 팝업 ui 추가
    확인용으로  open true 설정.(닫기 버튼 O)

---
+++ src/components/signin/button/CloseButton.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import Image from 'next/image';
+import React from 'react';
+import { HiOutlineX } from 'react-icons/hi';
+
+type Props = {
+  close: () => void;
+};
+
+export default function CloseButton({ close }: Props) {
+  return (
+    <button className="flex justify-center items-center w-4 h-4 bg-gray-200 rounded-full" onClick={close}>
+      <HiOutlineX className="text-black w-3 h-3" />
+    </button>
+  );
+}
