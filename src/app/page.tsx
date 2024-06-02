661bfb30c4f2d865e0258d37311bdd74908a2cd7
minhee <jmh3699@naver.com>
2024-06-02 11:57:27 +0900

    랜딩 페이지 및 모달 UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import Image from 'next/image';
 import Header from './components/Header';
+import MainMap from './components/MainMap';
+import LandingModal from './components/modal/Landing';
 
 export default function Home() {
   return (
     <>
       <Header />
-      <main />
+      <MainMap />
+      <LandingModal />
     </>
   );
 }
