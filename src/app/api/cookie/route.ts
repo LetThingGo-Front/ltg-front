a37bd9961e7945ecfcfe0551ca23439c9be55dba
leeduyeong <dy921119@gmail.com>
2024-09-12 16:53:11 +0900

    https 로그인 테스트 api 수정

--- src/app/api/cookie/route.ts
+++ src/app/api/cookie/route.ts
@@ -3,8 +3,16 @@ import { cookies } from 'next/headers';
 
 export async function GET(request: NextRequest) {
   // 쿠키에서 Access Token 가져오기
-  const cookieStore = cookies();
-  const accessToken = cookieStore.get('accessToken');
+  const cookies = request.headers.get('cookie') || '';
+
+  function getCookieValue(cookieHeader: string, cookieName: string): string | undefined {
+    const cookieArray = cookieHeader.split(';').map(cookie => cookie.trim());
+    const cookie = cookieArray.find(cookie => cookie.startsWith(`${cookieName}=`));
+    return cookie ? cookie.split('=')[1] : undefined;
+  }
+
+  // 'accessToken' 쿠키 값을 추출
+  const accessToken = getCookieValue(cookies, 'accessToken');
   console.log(accessToken);
   if (!accessToken) {
     return NextResponse.json(
