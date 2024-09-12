6b38f43be984f3ac9168b9aa3ba72cad0914b735
leeduyeong <dy921119@gmail.com>
2024-09-12 16:42:00 +0900

    https 로그인 테스트
    next js  api에서 쿠키 불러오기

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/:path*',
-        destination: 'https://letthinggo.duckdns.org/:path*',
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
