c789e1c0f755fa15dbee1fa0a7465c30a2eb9e5f
leeduyeong <dy921119@gmail.com>
2024-10-08 14:46:45 +0900

    axios config  base url 추가

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -4,7 +4,10 @@ const BASE_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
 
 export default axios.create({
   // baseURL: BASE_URL,
-  headers: { 'Content-Type': 'application/json' },
+  headers: {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': BASE_URL ?? '',
+  },
   withCredentials: true,
 });
 
