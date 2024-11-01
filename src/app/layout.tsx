9bae2a406d7c1159ebb5af01b72388e9dad5401a
leeduyeong <dy921119@gmail.com>
2024-11-01 13:45:25 +0900

    headconfig 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,7 @@ import InitToken from "@/components/common/InitToken";
 import { cookies } from "next/headers";
 import ReactQueryProviders from "@/provider/ReactQueryProviders";
 import splashScreens from "./splashScreens";
+import HeadConfig from "./HeadConfig";
 
 const pretendard = localFont({
   src: "./fonts/PretendardVariable.woff2",
@@ -19,13 +20,6 @@ const pretendard = localFont({
 export const metadata: Metadata = {
   title: "렛띵고",
   description: "Let your things go 묵혀두지 말고 보내주세요. 무료 나눔 서비스",
-  manifest: "/manifest.webmanifest",
-  appleWebApp: {
-    capable: true,
-    title: "렛띵고",
-    statusBarStyle: "black-translucent",
-    startupImage: splashScreens,
-  },
 };
 
 export const viewport: Viewport = {
@@ -48,6 +42,7 @@ export default function RootLayout({
 
   return (
     <html lang="en">
+      <HeadConfig />
       <body className={pretendard.className}>
         <ReactQueryProviders>
           <MapsProvider clientId={clientId}>
