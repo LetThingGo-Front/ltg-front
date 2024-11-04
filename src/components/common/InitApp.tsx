cb1506b8fcbd0a9d85d473ca4c037f2864c1791e
leeduyeong <dy921119@gmail.com>
2024-11-04 17:22:47 +0900

    화면 리사이징 이벤트 변경

--- src/components/common/InitApp.tsx
+++ src/components/common/InitApp.tsx
@@ -11,21 +11,33 @@ export default function InitApp({ token }: CommonProps) {
     await axiosAuth.delete("/v1/cookie/access-token");
   };
   useEffect(() => {
-    const handleResize = () => {
-      if (window.visualViewport) {
-        document.body.style.height = `${window.visualViewport.height}px`;
-      }
-    };
     if (window.visualViewport) {
-      window.visualViewport.addEventListener("resize", handleResize);
+      window.visualViewport.addEventListener("resize", () => {
+        document.body.style.position = "fixed"; // 스크롤 막기
+        document.body.style.top = `-${window.scrollY}px`; // 현재 스크롤 위치 고정
+      });
+
+      window.visualViewport.addEventListener("scroll", () => {
+        // 키보드에 의해 발생하는 자동 스크롤 방지
+        document.body.style.top = `-${window.scrollY}px`;
+      });
     }
+
+    // 키보드가 닫히면 스크롤 원상복구
+    window.addEventListener("blur", () => {
+      if (document.body.style.position === "fixed") {
+        const scrollPosition = Math.abs(parseInt(document.body.style.top, 10));
+        document.body.style.position = "";
+        document.body.style.top = "";
+        window.scrollTo(0, scrollPosition); // 원래 위치로 복구
+      }
+    });
 
     setupInterceptor();
     if (token) {
       utils.setStorage("accessToken", token);
       deleteCookieAccessToken();
     }
-    handleResize();
   }, []);
 
   return null;
