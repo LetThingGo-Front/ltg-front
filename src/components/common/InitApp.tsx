9fda1dc608bece275197cbb5f5cd88927409f662
leeduyeong <dy921119@gmail.com>
2024-11-04 17:27:17 +0900

    화면 리사이징 로직 제거

--- src/components/common/InitApp.tsx
+++ src/components/common/InitApp.tsx
@@ -11,28 +11,6 @@ export default function InitApp({ token }: CommonProps) {
     await axiosAuth.delete("/v1/cookie/access-token");
   };
   useEffect(() => {
-    if (window.visualViewport) {
-      window.visualViewport.addEventListener("resize", () => {
-        document.body.style.position = "fixed"; // 스크롤 막기
-        document.body.style.top = `-${window.scrollY}px`; // 현재 스크롤 위치 고정
-      });
-
-      window.visualViewport.addEventListener("scroll", () => {
-        // 키보드에 의해 발생하는 자동 스크롤 방지
-        document.body.style.top = `-${window.scrollY}px`;
-      });
-    }
-
-    // 키보드가 닫히면 스크롤 원상복구
-    window.addEventListener("blur", () => {
-      if (document.body.style.position === "fixed") {
-        const scrollPosition = Math.abs(parseInt(document.body.style.top, 10));
-        document.body.style.position = "";
-        document.body.style.top = "";
-        window.scrollTo(0, scrollPosition); // 원래 위치로 복구
-      }
-    });
-
     setupInterceptor();
     if (token) {
       utils.setStorage("accessToken", token);
