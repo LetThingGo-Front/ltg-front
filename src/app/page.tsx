002523ec7a82cec5a3f856e6934b7ce4fc8e84cd
leeduyeong <dy921119@gmail.com>
2024-11-06 13:28:33 +0900

    Fix: 메인 전체 frame 높이 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import LandingModal from "@/components/modal/Landing";
 
 export default function page() {
   return (
-    <div className="flex h-[calc(100%-5.375rem)] justify-center pt-[0.875rem] sm:h-[calc(100%-9.375rem)] sm:overflow-y-auto sm:pt-[4.25rem]">
-      <div className="relative mx-5 h-full w-full sm:mx-10 sm:min-h-[42.5rem]">
+    <div className="flex h-full justify-center pb-[1.375rem] pt-[0.875rem] sm:overflow-y-auto sm:pb-[5.125rem] sm:pt-[4.25rem]">
+      <div className="relative mx-5 h-full w-full sm:mx-10 sm:min-h-[41.875rem]">
         <div className="absolute left-0 top-0 h-full w-full rounded-[1.875rem]">
           <MainMap />
         </div>
--- src/app/product/register/page.tsx
+++ src/app/product/register/page.tsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function page() {
   return (
     <div className="flex h-full justify-center overflow-y-scroll px-[1.875rem] scrollbar-hide">
-      <div className="w-full sm:w-[37.5rem]">
+      <div className="w-full sm:max-w-[37.5rem]">
         <div className="mb-8 h-[4.6875rem] w-full p-5 max-sm:hidden">
           <p className="text-center text-2xl font-bold text-grey-800">
             나눔 등록
