04d7dd525c99a315364aeef25d4d012bd2588052
leeduyeong <dy921119@gmail.com>
2024-11-06 10:07:44 +0900

    Fix: landing page frame 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import LandingModal from "@/components/modal/Landing";
 
 export default function page() {
   return (
-    <div className="flex h-[calc(100%-6rem)] justify-center sm:h-[calc(100%-9.875rem)] sm:overflow-y-auto">
-      <div className="relative mx-5 h-full w-full sm:mx-10 sm:min-h-[47.0625rem] sm:pt-[4.25rem]">
+    <div className="flex h-[calc(100%-5.375rem)] justify-center pt-[0.875rem] sm:h-[calc(100%-9.375rem)] sm:overflow-y-auto sm:pt-[4.25rem]">
+      <div className="relative mx-5 h-full w-full sm:mx-10 sm:min-h-[42.5rem]">
         <div className="absolute left-0 top-0 h-full w-full rounded-[1.875rem]">
           <MainMap />
         </div>
