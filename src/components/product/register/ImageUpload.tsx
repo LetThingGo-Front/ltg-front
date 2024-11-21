984705f4503f7724e855b45e15a1b5fab3cffc48
leeduyeong <dy921119@gmail.com>
2024-11-21 14:34:35 +0900

    Fix: 이미지 업로드 파일 변환, 압축 함수 Promise allSettled 반환 타입 변경( 멀티 업로드 시 failed 이미지 구분 목적)

--- src/components/product/register/ImageUpload.tsx
+++ src/components/product/register/ImageUpload.tsx
@@ -27,58 +27,74 @@ export default function ImageUpload({ onChange }: Props) {
       useWebWorker: true,
     };
 
-    try {
-      const compressedFiles = await Promise.all(
-        file.map(async (f) => {
-          if (f.size > MAX_FILE_SIZE_MB * 1000000) {
-            const compressedBlob = await imageCompression(f, options);
-            const compressedFile = new File([compressedBlob], f.name, {
-              type: f.type,
-            });
-            return compressedFile;
-          } else {
-            return f;
-          }
-        }),
-      );
-      return compressedFiles;
-    } catch (error) {
-      setResizing(false);
-      console.error("image resizing error: ", error);
+    const compressedFiles = await Promise.allSettled(
+      file.map(async (f) => {
+        if (f.size > MAX_FILE_SIZE_MB * 1000000) {
+          const compressedBlob = await imageCompression(f, options);
+          const compressedFile = new File([compressedBlob], f.name, {
+            type: f.type,
+          });
+          return compressedFile;
+        } else {
+          return f;
+        }
+      }),
+    );
+    const allFailed = compressedFiles.every(
+      (file) => file.status === "rejected",
+    );
+    const isFailed = compressedFiles.some((file) => file.status === "rejected");
+
+    if (allFailed) {
       alert("이미지 압축 중 오류가 발생했습니다.");
     }
+    if (!allFailed && isFailed) {
+      alert("일부 이미지 압축 중 오류가 발생했습니다.");
+    }
+
+    return compressedFiles
+      .filter((file) => file.status === "fulfilled")
+      .map((file) => file.value);
   }, []);
 
   const convertHeicToType = useCallback(async (file: File[]) => {
-    try {
-      const heic2any = require("heic2any");
-      const convertedFiles = await Promise.all(
-        file.map(async (f) => {
-          const isHeic = /^.*\.(heic|heif)$/i;
-          if (isHeic.test(f.name)) {
-            const heicToJpeg: Blob = await heic2any({
-              blob: f,
-              toType: CONVERSION_TYPE,
-            });
-            return new File(
-              [heicToJpeg],
-              `${f.name.split(".")[0]}.${CONVERSION_FORMAT}`,
-              {
-                type: CONVERSION_TYPE,
-                lastModified: new Date().getTime(),
-              },
-            );
-          } else {
-            return f;
-          }
-        }),
-      );
-      return convertedFiles;
-    } catch (error) {
-      setResizing(false);
-      console.error("heic to jpg error: ", error);
+    const heic2any = require("heic2any");
+    const convertedFiles = await Promise.allSettled(
+      file.map(async (f) => {
+        const isHeic = /^.*\.(heic|heif)$/i;
+        if (isHeic.test(f.name)) {
+          const heicToJpeg: Blob = await heic2any({
+            blob: f,
+            toType: CONVERSION_TYPE,
+          });
+          return new File(
+            [heicToJpeg],
+            `${f.name.split(".")[0]}.${CONVERSION_FORMAT}`,
+            {
+              type: CONVERSION_TYPE,
+              lastModified: new Date().getTime(),
+            },
+          );
+        } else {
+          return f;
+        }
+      }),
+    );
+    const allFailed = convertedFiles.every(
+      (file) => file.status === "rejected",
+    );
+    const isFailed = convertedFiles.some((file) => file.status === "rejected");
+
+    if (allFailed) {
       alert("heic 이미지 변환 중 오류가 발생했습니다.");
     }
+    if (!allFailed && isFailed) {
+      alert("일부 heic 이미지 변환 중 오류가 발생했습니다.");
+    }
+
+    return convertedFiles
+      .filter((file) => file.status === "fulfilled")
+      .map((file) => file.value);
   }, []);
 
   const removeFile = useCallback(
@@ -108,9 +124,10 @@ export default function ImageUpload({ onChange }: Props) {
       setResizing(true);
       // heic to jpeg (브라우저 호환 및  압축 가능한 형식으로 변환)
       const convertedFiles = await convertHeicToType(acceptedFiles);
-      if (convertedFiles) {
+
+      if (convertedFiles.length > 0) {
         const compressedFiles = await compressImage(convertedFiles);
-        if (compressedFiles) {
+        if (compressedFiles.length > 0) {
           const newFiles = compressedFiles.map((file) =>
             Object.assign(file, {
               preview: URL.createObjectURL(file),
