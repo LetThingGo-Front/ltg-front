4f49e51cac17ad9b983643a9fb58cd41d482498e
leeduyeong <dy921119@gmail.com>
2024-12-05 10:03:45 +0900

    Feat: 로그인 시 바로 나눔 물품 등록 페이지로 이동 추가

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,17 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(req: NextRequest, res: NextResponse) {
+  const cookies = req.cookies;
+  const accessToken = cookies.get("accessToken")?.value;
+  console.log(accessToken);
+  if (accessToken) {
+    const { origin, basePath } = req.nextUrl;
+    const registerUrl = new URL(`${basePath}/product/register`, origin);
+    return NextResponse.redirect(registerUrl);
+  }
   return NextResponse.next();
 }
 
 export const config = {
-  // matcher: ['/product'],
+  matcher: ["/"],
 };
