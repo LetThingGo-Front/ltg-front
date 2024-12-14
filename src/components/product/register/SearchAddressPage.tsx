db41419f06d7f61ec5989291a3eea630c90160ba
leeduyeong <>
2024-12-14 14:55:52 +0900

    .

--- src/components/product/register/SearchAddressPage.tsx
+++ src/components/product/register/SearchAddressPage.tsx
@@ -99,61 +99,63 @@ export default function SearchAddressPage() {
   if (!isOpenSearchAddress) return null;
 
   return (
-    <div className="fixed left-0 top-[env(safe-area-inset-top)] z-30 h-[calc(100dvh-env(safe-area-inset-top))] w-full bg-white">
-      <div
-        className={clsx(
-          "flex h-16 w-full items-center justify-between overscroll-none bg-white px-5 py-[0.875rem]",
-        )}
-      >
-        <button onClick={closeSearchAddress} type="button">
-          <Image
-            src="/assets/images/button/arrow_left_2.svg"
-            width={32}
-            height={32}
-            alt="뒤로가기"
-          />
-        </button>
-        <div className="font-bold">주소 검색</div>
-        <div className="h-8 w-8"></div>
-      </div>
-      <div className="relative h-[2.75rem] bg-grey-50">
-        <div className={clsx("absolute left-3 top-[0.875rem] h-4 w-4")}>
-          <Image
-            className={clsx("pointerhover:group-hover/search:hidden")}
-            src="/assets/images/magnify.svg"
-            width={20}
-            height={20}
-            alt="search"
-          />
-        </div>
-        <input
-          className={clsx(
-            "ml-10 h-11 w-[calc(100%-4.75rem)] truncate bg-transparent text-[0.875rem] font-semibold text-grey-700 outline-none placeholder:text-center placeholder:text-[0.875rem]",
-          )}
-          ref={inputRef}
-          placeholder="주소를 검색하세요"
-          onChange={handleSearchInput}
-        />
-        <button
-          className={clsx("absolute right-3 top-4 h-3 w-3")}
-          type="button"
-          onClick={clearField}
-        >
-          <Image
-            src="/assets/images/button/close_grey.svg"
-            width={12}
-            height={12}
-            alt="close"
-          />
-        </button>
+    <div className="fixed left-0 top-[env(safe-area-inset-top)] z-30 h-[calc(100dvh-env(safe-area-inset-top))] w-full overflow-y-auto overscroll-none bg-white scrollbar-hide">
+      <div className="h-[calc(100dvh-env(safe-area-inset-top)+1px)]">
         <div
           className={clsx(
-            "absolute left-0 top-[2.75rem] max-h-[13.75rem] w-full bg-grey-50 sm:top-[2.75rem]",
+            "flex h-16 w-full items-center justify-between overscroll-none bg-white px-5 py-[0.875rem]",
           )}
         >
-          {searchList.map((addr, idx) => (
-            <AddAddressButton key={idx} {...addr} />
-          ))}
+          <button onClick={closeSearchAddress} type="button">
+            <Image
+              src="/assets/images/button/arrow_left_2.svg"
+              width={32}
+              height={32}
+              alt="뒤로가기"
+            />
+          </button>
+          <div className="font-bold">주소 검색</div>
+          <div className="h-8 w-8"></div>
+        </div>
+        <div className="relative h-[2.75rem] bg-grey-50">
+          <div className={clsx("absolute left-3 top-[0.875rem] h-4 w-4")}>
+            <Image
+              className={clsx("pointerhover:group-hover/search:hidden")}
+              src="/assets/images/magnify.svg"
+              width={20}
+              height={20}
+              alt="search"
+            />
+          </div>
+          <input
+            className={clsx(
+              "ml-10 h-11 w-[calc(100%-4.75rem)] truncate bg-transparent text-[0.875rem] font-semibold text-grey-700 outline-none placeholder:text-center placeholder:text-[0.875rem]",
+            )}
+            ref={inputRef}
+            placeholder="주소를 검색하세요"
+            onChange={handleSearchInput}
+          />
+          <button
+            className={clsx("absolute right-3 top-4 h-3 w-3")}
+            type="button"
+            onClick={clearField}
+          >
+            <Image
+              src="/assets/images/button/close_grey.svg"
+              width={12}
+              height={12}
+              alt="close"
+            />
+          </button>
+          <div
+            className={clsx(
+              "absolute left-0 top-[2.75rem] max-h-[13.75rem] w-full bg-grey-50 sm:top-[2.75rem]",
+            )}
+          >
+            {searchList.map((addr, idx) => (
+              <AddAddressButton key={idx} {...addr} />
+            ))}
+          </div>
         </div>
       </div>
     </div>
