7940371f2e37662720799ea0d221d7cc22eb4d7c
leeduyeong <dy921119@gmail.com>
2025-01-14 17:07:42 +0900

    build 오류 수정

--- src/mocks/handlers/itemHandler.ts
+++ src/mocks/handlers/itemHandler.ts
@@ -2,10 +2,6 @@ import { http, HttpResponse } from "msw";
 import itemData from "../data/item/itemData.json";
 
 export const itemHandler = [
-  http.post("/v1/items", async (request: Request) => {
-    const item = await request.json();
-    return HttpResponse.json({ status: "success", data: item });
-  }),
   http.get("/v1/items", async () => {
     const items: any = itemData;
     const responseItems = items.content.map((item: any) => {
